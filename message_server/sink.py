11772d4b6ed0e1798b050af12e25fd08e69e20df
Daniel Richman <main@danielrichman.co.uk>
2010-09-12 21:52:52 +0100

    Renamed reHAB -> habitat

--- message_server/sink.py
+++ message_server/sink.py
@@ -1,19 +1,19 @@
 # Copyright 2010 (C) Daniel Richman
 #
-# This file is part of reHAB.
+# This file is part of habitat.
 #
-# reHAB is free software: you can redistribute it and/or modify
+# habitat is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# reHAB is distributed in the hope that it will be useful,
+# habitat is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with reHAB.  If not, see <http://www.gnu.org/licenses/>.
+# along with habitat.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 Contains 'Sink', the parent class for all sinks.
