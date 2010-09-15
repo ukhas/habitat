8e74e0e51f5b5a2e73370d48f31f34a937560f53
Daniel Richman <main@danielrichman.co.uk>
2010-09-15 19:06:32 +0100

    Add sink types management (and a message validate_type function)

--- message_server/sink.py
+++ message_server/sink.py
@@ -17,7 +17,59 @@
 
 """
 Contains 'Sink', the parent class for all sinks.
+
+A 'Sink' has the following methods:
+  update the internal list of message-types which the sink wishes to receive
+    Sink.add_type(type), Sink.add_types(set([type, type, ...]))
+    Sink.remove_type(type), Sink.remove_types(set([type, type, ...]))
+    Sink.set_types(types), Sink.clear_types()
+
+  called internally by the message server
+    Sink.push_message(message)
+
+Currently, a sink must inherit this class and in addition define these 
+functions:
+start(): called once; the sink must call some of the self.*type* functions in
+         order to set up the set of types that the sink would like to receive
+message(message): called whenever a message is received for the sink to 
+                  process
+
+Please note that multiple calls to message() may be made simultaneously by
+different threads. Your message() function must not block!
 """
 
+from message import Message
+
 class Sink:
-    pass
+    def __init__(self):
+        self.types = set()
+
+        self.add_type = TypeValidator(self.types.add)
+        self.add_types = TypesValidator(self.types.update)
+        self.remove_type = TypeValidator(self.types.discard)
+        self.remove_types = TypesValidator(self.types.difference_update)
+        self.clear_types = self.types.clear
+
+    def set_types(self, types):
+        self.clear_types()
+        self.add_types(types)
+
+
+class Validator:
+    def __init__(self, func):
+        self.func = func
+
+class TypeValidator(Validator):
+    def __call__(self, type):
+        Message.validate_type(type)
+        self.func(type)
+
+class TypesValidator(Validator):
+    def __call__(self, types):
+        if not isinstance(types, (set, frozenset)):
+            raise TypeError("types must be a set")
+
+        for type in types:
+            Message.validate_type(type)
+
+        self.func(types)
--- message_server/tests/test_sink.py
+++ message_server/tests/test_sink.py
@@ -19,7 +19,118 @@
 Tests the Sink class, found in ../sink.py
 """ 
 
-class TestSink:
-    def test_can_be_imported(self):
-        from message_server import Sink
+from nose.tools import raises
+from message_server import Sink
+from message_server import Message
 
+class EmptySink(Sink):
+    def start(self):
+        pass
+
+    def message(self):
+        pass
+
+class FakeSink(Sink):
+    def start(self):
+        self.set_types(set([Message.RECEIVED_TELEM, Message.LISTENER_INFO]))
+        self.test_messages = []
+        self.message = self.test_messages.append
+
+class TestSink:
+    def test_types_is_a_set(self):
+        sink = EmptySink()
+        assert isinstance(sink.types, set)
+
+    def test_types(self):
+        sink = EmptySink()
+
+        # We need these executed in this precise order but it'd be nice if
+        # they were printed by spec as separate tests:
+        yield self.check_add_types, sink
+        yield self.check_remove_type_a, sink
+        yield self.check_add_type_a, sink
+        yield self.check_add_type_b, sink
+        yield self.check_add_type_c, sink
+        yield self.check_remove_types, sink
+        yield self.check_remove_type_b, sink
+        yield self.check_set_types, sink
+        yield self.check_clear_types, sink
+
+    def check_add_types(self, sink):
+        sink.add_types(set([Message.RECEIVED_TELEM, Message.LISTENER_INFO]))
+        assert sink.types == set([Message.RECEIVED_TELEM, 
+                                  Message.LISTENER_INFO])
+
+    def check_remove_type_a(self, sink):
+        sink.remove_type(Message.RECEIVED_TELEM)
+        assert sink.types == set([Message.LISTENER_INFO])
+
+    def check_add_type_a(self, sink):
+        sink.add_type(Message.TELEM)
+        assert sink.types == set([Message.LISTENER_INFO, Message.TELEM])
+
+    def check_add_type_b(self, sink):
+        sink.add_type(Message.TELEM)
+        assert sink.types == set([Message.LISTENER_INFO, Message.TELEM])
+
+    def check_add_type_c(self, sink):
+        sink.add_type(Message.RECEIVED_TELEM)
+        assert sink.types == set([Message.LISTENER_INFO, Message.TELEM,
+                                  Message.RECEIVED_TELEM])
+
+    def check_remove_types(self, sink):
+        sink.remove_types(set([Message.LISTENER_INFO, Message.TELEM]))
+        assert sink.types == set([Message.RECEIVED_TELEM])
+
+    def check_remove_type_b(self, sink):
+        sink.remove_type(Message.RECEIVED_TELEM)
+        assert sink.types == set([])
+
+    def check_set_types(self, sink):
+        sink.set_types(set([Message.LISTENER_INFO, Message.LISTENER_INFO,
+                            Message.LISTENER_TELEM]))
+        assert sink.types == set([Message.LISTENER_INFO,
+                                  Message.LISTENER_TELEM])
+
+    def check_clear_types(self, sink):
+        sink.clear_types()
+        assert sink.types == set([])
+
+    def test_can_remove_type_twice(self):
+        sink = EmptySink()
+        sink.add_type(Message.RECEIVED_TELEM)
+        sink.remove_type(Message.RECEIVED_TELEM)
+        sink.remove_type(Message.RECEIVED_TELEM)
+        # Should not produce a KeyError
+    
+    def test_rejects_garbage(self):
+        sink = EmptySink()
+        for i in [sink.add_type, sink.remove_type]:
+            yield self.check_rejects_garbage_type, i
+            yield self.check_rejects_invalid_type, i
+    
+        for i in [sink.add_types, sink.remove_types,
+                  sink.set_types]:
+            yield self.check_rejects_garbage_types, i
+            yield self.check_rejects_garbage_set, i
+            yield self.check_rejects_invalid_types, i
+    
+    @raises(TypeError)
+    def check_rejects_garbage_type(self, func):
+        func("asdf")
+    
+    @raises(TypeError)
+    def check_rejects_garbage_types(self, func):
+        func(set(["asdf", Message.RECEIVED_TELEM]))
+    
+    @raises(TypeError)
+    def check_rejects_garbage_set(self, func):
+        func(1337) # An int, not a set
+    
+    @raises(ValueError)
+    def check_rejects_invalid_type(self, func):
+        func(951)
+    
+    @raises(ValueError)
+    def check_rejects_invalid_types(self, func):
+        func(set([Message.RECEIVED_TELEM, 952]))
