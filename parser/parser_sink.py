3db87b6d6616515b80e9ec7dddff6b17b101180e
Adam Greig <random@randomskk.net>
2010-09-16 21:50:23 +0100

    Added empty ParserSink class

---
+++ parser/parser_sink.py
@@ -0,0 +1,32 @@
+# Copyright 2010 (C) Adam Greig
+#
+# This file is part of habitat.
+#
+# habitat is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# habitat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with habitat.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+The parser sink is the interface between the message server and the
+parser. It is responsible for receiving raw telemetry from the message
+server, turning it into beautiful telemetry data, and then sending that
+back.
+"""
+
+from message_server import SimpleSink
+
+class ParserSink(SimpleSink):
+    def setup(self):
+        pass
+    def message(self):
+        pass
+
---
+++ parser/tests/test_parser_sink.py
@@ -0,0 +1,31 @@
+# Copyright 2010 (C) Adam Greig
+#
+# This file is part of habitat.
+#
+# habitat is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# habitat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with habitat.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Unit tests for the Parser's Sink class.
+"""
+
+from nose.tools import raises
+from message_server import Server, Message
+from parser import ParserSink
+
+class TestParserSink:
+    def setup(self):
+        self.server = Server()
+
+    def test_sink_can_be_loaded(self):
+        self.server.load(ParserSink)
