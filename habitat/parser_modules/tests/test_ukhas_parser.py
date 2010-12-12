906ccfed0a0e8e1d0b7b2f6012ed5071a1a76d34
Adam Greig <random@randomskk.net>
2010-12-12 20:26:06 +0000

    Added comments explaining the test data

--- habitat/parser_modules/tests/test_ukhas_parser.py
+++ habitat/parser_modules/tests/test_ukhas_parser.py
@@ -19,6 +19,12 @@
 Test the UKHAS protocol parser.
 """
 
+from nose.tools import assert_raises
+from copy import deepcopy
+
+from habitat.parser_modules.ukhas_parser import UKHASParser
+
+# A 'standard' config. Other configs can copy this and change parts.
 base_config = {
     "protocol": "UKHAS",
     "checksum": "crc16-ccitt",
@@ -50,87 +56,106 @@ base_config = {
     ]
 }
 
-from nose.tools import assert_raises
-from copy import deepcopy
-
-from habitat.parser_modules.ukhas_parser import UKHASParser
-
+# Each of these is a totally invalid stub that should just fail.
+# The last one might be valid but has non-hexadecimal checksum characters.
 bad_sentences = ["", "bad", "$$bad*", "bad*CC", "bad*CCCC",
     "$$bad*GH", "$$bad,bad*GHIJ"]
 
+# Each of these short stubs should pass pre-parsing and return a callsign
 good_sentences = ["$$good,data", "$$good,data*CC", "$$good,data*CCCC",
         "$$good,lots,of,1234,5678.90,data$CCCC"]
 
+# A configuration without a protocol key (should fail)
 config_no_protocol = deepcopy(base_config)
 del config_no_protocol["protocol"]
 
+# A configuration with an invalid protocol key (should fail)
 config_invalid_protocol = deepcopy(base_config)
 config_invalid_protocol["protocol"] = "invalid"
 
+# A configuration without a checksum key (should fail)
 config_no_checksum = deepcopy(base_config)
 del config_no_checksum["checksum"]
 
+# A configuration without a fields dictionary (should fail)
 config_no_fields = deepcopy(base_config)
 del config_no_fields["fields"]
 
+# A configuration with an empty fields dictionary (should fail)
 config_empty_fields = deepcopy(base_config)
 config_empty_fields["fields"] = {}
 
+# A configuration where a field has no name (should fail)
 config_field_without_name = deepcopy(base_config)
 del config_field_without_name["fields"][0]["name"]
 
+# A configuration where a field has no type (should fail)
 config_field_without_type = deepcopy(base_config)
 del config_field_without_type["fields"][0]["type"]
 
+# A configuration where a coordinate field lacks a format (should fail)
 config_field_without_format = deepcopy(base_config)
 del config_field_without_format["fields"][2]["format"]
 
-config_checksum_crc16_ccitt = deepcopy(base_config)
-config_checksum_crc16_ccitt["checksum"] = "crc16-ccitt"
-
-config_checksum_xor = deepcopy(base_config)
-config_checksum_xor["checksum"] = "xor"
-
-config_checksum_fletcher_16 = deepcopy(base_config)
-config_checksum_fletcher_16["checksum"] = "fletcher-16"
-
-config_checksum_fletcher_16_256 = deepcopy(base_config)
-config_checksum_fletcher_16_256["checksum"] = "fletcher-16-256"
-
-config_checksum_none = deepcopy(base_config)
-config_checksum_none["checksum"] = "none"
-
+# A configuration with an invalid checksum (should fail)
 config_checksum_invalid = deepcopy(base_config)
 config_checksum_invalid = "invalid"
 
+# A configuration with an invalid field type (should fail)
 config_field_type_invalid = deepcopy(base_config)
 config_field_type_invalid["fields"][0]["type"] = "invalid"
 
+# A configuration with an invalid coordinate format (should fail)
 config_format_invalid = deepcopy(base_config)
 config_field_type_invalid["fields"][2]["format"] = "invalid"
 
+# A configuration with a CRC16-CCITT checksum
+config_checksum_crc16_ccitt = deepcopy(base_config)
+config_checksum_crc16_ccitt["checksum"] = "crc16-ccitt"
+
+# A configuration with an XOR checksum
+config_checksum_xor = deepcopy(base_config)
+config_checksum_xor["checksum"] = "xor"
+
+# A configuration with a Fletcher-16 checksum
+config_checksum_fletcher_16 = deepcopy(base_config)
+config_checksum_fletcher_16["checksum"] = "fletcher-16"
+
+# A configuration with a Fletcher-16 checksum, mod 256 (legacy)
+config_checksum_fletcher_16_256 = deepcopy(base_config)
+config_checksum_fletcher_16_256["checksum"] = "fletcher-16-256"
+
+# A configuration with no checksum
+config_checksum_none = deepcopy(base_config)
+config_checksum_none["checksum"] = "none"
+
+# A configuration with coordinates in degrees and minutes
 config_minutes = deepcopy(base_config)
 config_minutes["fields"][2]["format"] = "ddmm.mm"
 config_minutes["fields"][3]["format"] = "ddmm.mm"
 
+# Valid sentences with various valid checksums
 sentence_no_checksum = "$$habitat,1,00:00:00,0.0,0.0,0,0.0,hab"
 sentence_crc16_ccitt = "$$habitat,1,00:00:00,0.0,0.0,0,0.0,hab*EE5E"
 sentence_xor = "$$habitat,1,00:00:00,0.0,0.0,0,0.0,hab*0b"
 sentence_fletcher_16 = "$$habitat,1,00:00:00,0.0,0.0,0,0.0,hab*e3a6"
 sentence_fletcher_16_256 = "$$habitat,1,00:00:00,0.0,0.0,0,0.0,hab*DBF5"
 
+# Valid sentences with various incorrect checksums
 sentence_bad_crc16_ccitt = "$$habitat,1,00:00:00,0.0,0.0,0,0.0,hab*abcd"
 sentence_bad_xor = "$$habitat,1,00:00:00,0.0,0.0,0,0.0,hab*9g"
 sentence_bad_fletcher_16 = "$$habitat,1,00:00:00,0.0,0.0,0,0.0,hab*abcd"
 sentence_bad_fletcher_16_256 = "$$habitat,1,00:00:00,0.0,0.0,0,0.0,hab*dcba"
 
+# Sentences with invalid field values
 sentence_bad_int = "$$habitat,a,00:00:00,0.0,0.0,0,0.0,hab"
 sentence_bad_time = "$$habitat,1,aa:bb:cc,0.0,0.0,0,0.0,hab"
 sentence_bad_time_2 = "$$habitat,1,123,0.0,0.0,0,0.0,hab"
 sentence_bad_float = "$$habitat,1,00:00:00,abc,0.0,0,0.0,hab"
-
 sentence_bad_minutes = "$$habitat,1,00:00:00,087.123,0.0,0,0.0,hab"
 
+# Several examples of valid sentences, where the coordinates are variously
+# mangled (in minutes, or with funny padding, random 0s and spaces
 sentence_good = "$$habitat,123,12:45:06,-35.1032,138.8568,4285,3.6,hab*5681"
 sentence_good_2 = "$$habitat,123,12:45,-35.1032,138.8568,4285,3.6,hab*8e78"
 sentence_good_3 = \
@@ -142,25 +167,32 @@ sentence_good_5 = \
 sentence_good_6 = \
     "$$habitat,123,12:45:06,035.1032,0138.8568,4285,3.6,hab*D856"
 
+# Correct parser output for the checksum test sentences
 output_checksum_test = {
     "payload": "habitat", "message_count": 1,
     "time": {"hour": 0, "minute": 0, "second": 0},
     "latitude": 0.0, "longitude": 0.0, "altitude": 0,
     "speed": 0.0, "custom_string": "hab"}
 
+# Correct parser output for (most) of the good sentences
 output_good = {
     "payload": "habitat", "message_count": 123,
     "time": {"hour": 12, "minute": 45, "second": 06},
     "latitude": -35.1032, "longitude": 138.8568,
     "altitude": 4285, "speed": 3.6, "custom_string": "hab"}
 
+# Correct parser output for sentence_good_2 (no seconds on the time)
 output_good_2 = deepcopy(output_good)
 del output_good_2["time"]["second"]
 
+# Correct parser output for sentence_good_6 (positive latitude)
 output_good_6 = deepcopy(output_good)
 output_good_6["latitude"] = 35.1032
 
 class M:
+    """Store the constructor argument in self.data, to fake being a message
+    for the purposes of testing the parser modules
+    """
     def __init__(self, data):
         self.data = data
 
