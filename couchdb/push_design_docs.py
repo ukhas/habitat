8bfad88801a945998edbb7feee2a341d93113a62
Adam Greig <adam@adamgreig.com>
2011-01-14 17:33:04 +0000

    Added default document population of database

--- couchdb/push_design_docs.py
+++ couchdb/push_design_docs.py
@@ -15,8 +15,11 @@
 # You should have received a copy of the GNU General Public License
 # along with habitat.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+import json
 from couchdbkit import Server
 from couchdbkit.designer import push
+from couchdbkit import exceptions
 
 # Find out the URI and DB to connect to
 uri = raw_input("CouchDB URI [http://localhost:5984]: ")
@@ -31,5 +34,20 @@ if db == "":
 s = Server(uri)
 db = s[db]
 
-# Push documents up
+# Load sample documents if they don't exist
+docs = os.listdir("docs/")
+for doc in docs:
+    f = open("docs/" + doc)
+    doc = json.loads(f.read())
+    f.close()
+    try:
+        db.save_doc(doc)
+    except exceptions.ResourceConflict:
+        print "Document <" + doc["_id"] + "> already in database, skipping."
+        continue
+
+# Push habitat design documents (this is done second as the design
+# doc prevents the creation of non-flight documents by anything except
+# the habitat user)
 push("habitat", db)
+
