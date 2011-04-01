62b1cfb078d808ad03e54af49781c865b2bad128
Adam Greig <adam@adamgreig.com>
2011-04-02 00:26:24 +0100

    list_telem works. kinda. it does something.

--- web/js/list_telem.js
+++ web/js/list_telem.js
@@ -2,7 +2,9 @@ function list_telem() {
     $db = $.couch.db("habitat");
     $db.view("habitat/payload_telem",
         {"limit": "20", "descending": "true", success: function(data) {
-            $("#telems").html(data);
+            for(var i = 0; i < data.rows.length; i++) {
+                $("#telems").append(data.rows[i].id);
+                $("#telems").append("<br />");
         }});
 };
 
