f9d8a53eb2ec1b3b086c4ef066bc61532a2e8f4d
Daniel Richman <main@danielrichman.co.uk>
2011-08-10 22:23:26 +0100

    Added bare bones uploader module

---
+++ uploader/__init__.py
@@ -0,0 +1,23 @@
+# Copyright 2011 (C) Daniel Richman
+#
+# This file is part of habitat.
+#
+# habitat is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# habitat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with habitat.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+The uploader is a client for end users that pushes documents into a couchdb
+database where they can be used directly by the web client or picked up
+by a daemon for further processing.
+"""
+
