56da4119368e774dc99216ff0a4bb147429572d6
Daniel Richman <main@danielrichman.co.uk>
2011-08-11 23:56:27 +0100

    Added payload telemetry merging

--- habitat/uploader/tests/test_uploader.py
+++ habitat/uploader/tests/test_uploader.py
@@ -20,6 +20,7 @@ Tests for the uploader module
 """
 
 import mox
+import copy
 import uuid
 
 import time
@@ -71,6 +72,29 @@ payload_telemetry_doc = {
         }
     }
 }
+payload_telemetry_doc_existing = {
+    "data": {
+        "_raw": "YXNkZiBibGFoIBIgYmluYXIEAA==",
+        "some_parsed_data": 12345
+    },
+    "type": "payload_telemetry",
+    "receivers": {
+        "SOMEONEELSE": {
+            "time_created": 200,
+            "time_uploaded": 240,
+            "frequency": 434074000,
+            "asdf": "World"
+        }
+    }
+}
+payload_telemetry_doc_merged = copy.deepcopy(payload_telemetry_doc_existing)
+payload_telemetry_doc_merged["receivers"]["TESTCALL"] = \
+    copy.deepcopy(payload_telemetry_doc["receivers"]["TESTCALL"])
+payload_telemetry_doc_merged["receivers"]["TESTCALL"]["time_uploaded"] += 1
+payload_telemetry_doc_existing_collision = \
+    copy.deepcopy(payload_telemetry_doc_existing)
+payload_telemetry_doc_existing_collision["data"]["_raw"] = "cGluZWFwcGxlcw=="
+
 payload_telemetry_doc_id = "cf4511bba32c4273a13d8f2e39501a96" \
                            "9ec664a4dc5c67bc556b514410087309"
 
@@ -141,9 +165,11 @@ class TestUploader(object):
         self.docs[doc_id] = doc
 
     def add_sample_listener_docs(self):
-        uploader.time.time().AndReturn(1234)
+        uploader.time.time().AndReturn(1233.9)
+        uploader.time.time().AndReturn(1234.0)
         self.fake_db.save_doc(telemetry_doc).WithSideEffects(self.save_doc)
-        uploader.time.time().AndReturn(1259)
+        uploader.time.time().AndReturn(1259.1)
+        uploader.time.time().AndReturn(1259.1)
         self.fake_db.save_doc(info_doc).WithSideEffects(self.save_doc)
         self.mocker.ReplayAll()
 
@@ -155,19 +181,20 @@ class TestUploader(object):
     def test_pushes_listener_docs(self):
         self.add_sample_listener_docs()
 
-        uploader.time.time().AndReturn(1276)
+        uploader.time.time().AndReturn(1276.1)
         self.fake_db.save_doc(telemetry_time_doc) \
                 .WithSideEffects(self.save_doc)
-        uploader.time.time().AndReturn(1290)
+        uploader.time.time().AndReturn(1290.1)
         self.fake_db.save_doc(info_time_doc).WithSideEffects(self.save_doc) 
         self.mocker.ReplayAll()
 
         self.uploader.listener_telemetry(telemetry_data, time_created=1232)
-        self.uploader.listener_info(info_data, time_created=1254)
+        self.uploader.listener_info(info_data, time_created=1253.8)
         self.mocker.VerifyAll()
 
     def test_pushes_payload_telemetry_simple(self):
-        uploader.time.time().AndReturn(1234)
+        uploader.time.time().AndReturn(1234.3)
+        uploader.time.time().AndReturn(1234.3)
         self.fake_db.__setitem__(payload_telemetry_doc_id,
                                  payload_telemetry_doc)
         self.mocker.ReplayAll()
@@ -198,7 +225,8 @@ class TestUploader(object):
     def test_adds_latest_listener_docs(self):
         self.add_sample_listener_docs()
 
-        uploader.time.time().AndReturn(1234)
+        uploader.time.time().AndReturn(1234.0)
+        uploader.time.time().AndReturn(1234.0)
         self.fake_db.__setitem__(payload_telemetry_doc_id, 
                                  mox.Func(self.ptlm_with_listener_docs))
         self.mocker.ReplayAll()
@@ -207,4 +235,94 @@ class TestUploader(object):
                                         payload_telemetry_metadata)
         self.mocker.VerifyAll()
 
-        # TODO: conflict resolution
+    def test_ptlm_merges_payload_conflicts(self):
+        uploader.time.time().AndReturn(1234.0)
+        uploader.time.time().AndReturn(1234.0)
+        self.fake_db.__setitem__(payload_telemetry_doc_id,
+                                 payload_telemetry_doc) \
+             .AndRaise(couchdbkit.exceptions.ResourceConflict)
+        self.fake_db.__getitem__(payload_telemetry_doc_id) \
+             .AndReturn(payload_telemetry_doc_existing)
+        uploader.time.time().AndReturn(1235.0)
+        self.fake_db.__setitem__(payload_telemetry_doc_id,
+                                 payload_telemetry_doc_merged)
+        self.mocker.ReplayAll()
+
+        self.uploader.payload_telemetry(payload_telemetry_string,
+                                        payload_telemetry_metadata)
+        self.mocker.VerifyAll()
+
+    def test_ptlm_refuses_to_merge_collision(self):
+        uploader.time.time().AndReturn(1234.0)
+        uploader.time.time().AndReturn(1234.0)
+        self.fake_db.__setitem__(payload_telemetry_doc_id,
+                                 payload_telemetry_doc) \
+             .AndRaise(couchdbkit.exceptions.ResourceConflict)
+        self.fake_db.__getitem__(payload_telemetry_doc_id) \
+             .AndReturn(payload_telemetry_doc_existing_collision)
+        uploader.time.time().AndReturn(1235.0)
+        self.mocker.ReplayAll()
+
+        try:
+            self.uploader.payload_telemetry(payload_telemetry_string,
+                                            payload_telemetry_metadata)
+        except uploader.Collision:
+            pass
+        else:
+            raise AssertionError("Did not raise Collision")
+
+        self.mocker.VerifyAll()
+
+    def add_mock_conflicts(self, n):
+        uploader.time.time().AndReturn(1234.0)
+        uploader.time.time().AndReturn(1234.0)
+        self.fake_db.__setitem__(payload_telemetry_doc_id,
+                                 payload_telemetry_doc) \
+             .AndRaise(couchdbkit.exceptions.ResourceConflict)
+
+        doc = payload_telemetry_doc_existing
+        doc_merged = payload_telemetry_doc_merged
+
+        for i in xrange(n):
+            self.fake_db.__getitem__(payload_telemetry_doc_id).AndReturn(doc)
+            uploader.time.time().AndReturn(1235.0 + i)
+            self.fake_db.__setitem__(payload_telemetry_doc_id, doc_merged) \
+                .AndRaise(couchdbkit.exceptions.ResourceConflict)
+
+            doc = copy.deepcopy(doc)
+            doc_merged = copy.deepcopy(doc_merged)
+
+            new_call = "listener_{0}".format(i)
+            new_info = {"time_created": 1000 + i, "time_uploaded": 1001 + i}
+            doc["receivers"][new_call] = new_info
+            doc_merged["receivers"][new_call] = new_info
+            doc_merged["receivers"]["TESTCALL"]["time_uploaded"] += 1
+
+        return (doc, doc_merged)
+
+    def test_merges_multiple_conflicts(self):
+        (final_doc, final_doc_merged) = self.add_mock_conflicts(14)
+
+        self.fake_db.__getitem__(payload_telemetry_doc_id).AndReturn(final_doc)
+        uploader.time.time().AndReturn(1235.0 + 14)
+        self.fake_db.__setitem__(payload_telemetry_doc_id, final_doc_merged)
+        self.mocker.ReplayAll()
+
+        self.uploader.payload_telemetry(payload_telemetry_string,
+                                        payload_telemetry_metadata)
+
+        self.mocker.VerifyAll()
+
+    def test_gives_up_after_many_conflicts(self):
+        self.add_mock_conflicts(20)
+        self.mocker.ReplayAll()
+
+        try:
+            self.uploader.payload_telemetry(payload_telemetry_string,
+                                            payload_telemetry_metadata)
+        except uploader.Unmergeable:
+            pass
+        else:
+            raise AssertionError("Did not raise Unmergeable")
+
+        self.mocker.VerifyAll()
