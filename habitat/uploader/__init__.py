01d682135b991f5d5f2e7b04eaecbac21b70d971
Daniel Richman <main@danielrichman.co.uk>
2011-08-11 22:42:18 +0100

    Add latest_listener_{info,telemetry} support

--- habitat/uploader/__init__.py
+++ habitat/uploader/__init__.py
@@ -22,6 +22,7 @@ by a daemon for further processing.
 """
 
 import time
+import copy
 import base64
 import hashlib
 import couchdbkit
@@ -31,41 +32,53 @@ class Uploader(object):
                        couch_uri="http://habhub.org/",
                        couch_db="habitat"):
         self._callsign = callsign
+        self._latest = {}
 
         server = couchdbkit.Server(couch_uri)
         self._db = server[couch_db]
 
-    def listener_telem(self, data, time_created=None):
-        self._listener_doc(data, "listener_telem", time_created)
+    def listener_telemetry(self, data, time_created=None):
+        self._listener_doc(data, "listener_telemetry", time_created)
 
     def listener_info(self, data, time_created=None):
         self._listener_doc(data, "listener_info", time_created)
 
-    def _listener_doc(self, data, type, time_created=None):
+    def _listener_doc(self, data, doc_type, time_created=None):
         time_uploaded = time.time()
 
         if time_created is None:
             time_created = time_uploaded
 
         doc = {
-            "data": data,
-            "type": type,
+            "data": copy.deepcopy(data),
+            "type": doc_type,
             "time_created": time_created,
             "time_uploaded": time_uploaded
         }
 
         self._db.save_doc(doc)
 
+        doc_id = doc["_id"]
+        self._latest[doc_type] = doc_id
+
     def payload_telemetry(self, string, metadata, time_created=None):
+        for key in ["time_created", "time_uploaded", "latest_listener_info",
+                    "latest_listener_telemetry"]:
+            assert key not in metadata
+
         time_uploaded = time.time()
 
         if time_created is None:
             time_created = time_uploaded
 
-        receiver_info = {
-            "time_created": time_created,
-            "time_uploaded": time_uploaded,
-        }
+        receiver_info = copy.deepcopy(metadata)
+
+        receiver_info["time_created"] = time_created
+        receiver_info["time_uploaded"] = time_uploaded
+
+        for doc_type in ["listener_telemetry", "listener_info"]:
+            if doc_type in self._latest:
+                receiver_info["latest_" + doc_type] = self._latest[doc_type]
 
         doc = {
             "data": {"_raw": base64.b64encode(string)},
