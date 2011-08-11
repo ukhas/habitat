234be558f0688f1373fb9729c4975bad3e4882a5
Daniel Richman <main@danielrichman.co.uk>
2011-08-11 22:42:18 +0100

    Add latest_listener_{info,telemetry} support

--- habitat/uploader/tests/test_uploader.py
+++ habitat/uploader/tests/test_uploader.py
@@ -20,11 +20,59 @@ Tests for the uploader module
 """
 
 import mox
+import uuid
 
 import time
 import couchdbkit
 
 from ... import uploader
+
+telemetry_data = {"some_data": 123, "_flag": True}
+telemetry_doc = {
+    "data": telemetry_data,
+    "type": "listener_telemetry",
+    "time_created": 1234,
+    "time_uploaded": 1234,
+}
+telemetry_time_doc = {
+    "data": telemetry_data,
+    "type": "listener_telemetry",
+    "time_created": 1232,
+    "time_uploaded": 1276
+}
+
+info_data = {"my_radio": "Duga-3", "vehicle": "Tractor"}
+info_doc = {
+    "data": info_data,
+    "type": "listener_info",
+    "time_created": 1259,
+    "time_uploaded": 1259
+}
+info_time_doc = {
+    "data": info_data,
+    "type": "listener_info",
+    "time_created": 1254,
+    "time_uploaded": 1290
+}
+
+payload_telemetry_string = "asdf blah \x12 binar\x04\x00"
+payload_telemetry_metadata = {"frequency": 434075000, "misc": "Hi"}
+payload_telemetry_doc = {
+    "data": {
+        "_raw": "YXNkZiBibGFoIBIgYmluYXIEAA=="
+    },
+    "type": "payload_telemetry",
+    "receivers": {
+        "TESTCALL": {
+            "time_created": 1234,
+            "time_uploaded": 1234,
+            "frequency": 434075000, # TODO: this ought to be in the schema/docs
+            "misc": "Hi"
+        }
+    }
+}
+payload_telemetry_doc_id = "cf4511bba32c4273a13d8f2e39501a96" \
+                           "9ec664a4dc5c67bc556b514410087309"
 
 class TestUploaderSetup(object):
     def setup(self):
@@ -77,66 +125,86 @@ class TestUploader(object):
         fake_server.__getitem__("habitat").AndReturn(self.fake_db)
 
         self.mocker.ReplayAll()
-        self.uploader = uploader.Uploader("TESTCALL", 
+        self.uploader = uploader.Uploader("TESTCALL",
                                           couch_uri="http://server/")
         self.mocker.VerifyAll()
         self.mocker.ResetAll()
 
+        self.docs = {}
+
     def teardown(self):
         self.mocker.UnsetStubs()
 
-    def test_pushes_listener_docs(self):
-        telem_data = {"some_data": 123, "_flag": True}
-        telem_doc = {"data": telem_data, "type": "listener_telem",
-                     "time_created": 1234, "time_uploaded": 1234}
-        telem_time_doc = {"data": telem_data, "type": "listener_telem",
-                          "time_created": 1232, "time_uploaded": 1276}
-        info_data = {"my_radio": "Duga-3", "vehicle": "Tractor"}
-        info_doc = {"data": info_data, "type": "listener_info",
-                    "time_created": 1259, "time_uploaded": 1259}
-        info_time_doc = {"data": info_data, "type": "listener_info",
-                         "time_created": 1254, "time_uploaded": 1290}
+    def save_doc(self, doc):
+        doc_id = str(uuid.uuid1())
+        doc["_id"] = doc_id
+        self.docs[doc_id] = doc
 
+    def add_sample_listener_docs(self):
         uploader.time.time().AndReturn(1234)
-        self.fake_db.save_doc(telem_doc)
+        self.fake_db.save_doc(telemetry_doc).WithSideEffects(self.save_doc)
         uploader.time.time().AndReturn(1259)
-        self.fake_db.save_doc(info_doc)
-        uploader.time.time().AndReturn(1276)
-        self.fake_db.save_doc(telem_time_doc)
-        uploader.time.time().AndReturn(1290)
-        self.fake_db.save_doc(info_time_doc)
+        self.fake_db.save_doc(info_doc).WithSideEffects(self.save_doc)
         self.mocker.ReplayAll()
 
-        self.uploader.listener_telem(telem_data)
+        self.uploader.listener_telemetry(telemetry_data)
         self.uploader.listener_info(info_data)
-        self.uploader.listener_telem(telem_data, time_created=1232)
+        self.mocker.VerifyAll()
+        self.mocker.ResetAll()
+
+    def test_pushes_listener_docs(self):
+        self.add_sample_listener_docs()
+
+        uploader.time.time().AndReturn(1276)
+        self.fake_db.save_doc(telemetry_time_doc) \
+                .WithSideEffects(self.save_doc)
+        uploader.time.time().AndReturn(1290)
+        self.fake_db.save_doc(info_time_doc).WithSideEffects(self.save_doc) 
+        self.mocker.ReplayAll()
+
+        self.uploader.listener_telemetry(telemetry_data, time_created=1232)
         self.uploader.listener_info(info_data, time_created=1254)
         self.mocker.VerifyAll()
 
     def test_pushes_payload_telemetry_simple(self):
-        string = "asdf blah \x12 binar\x04\x00"
-        metadata = {"frequency": 434075000, "somethingelse": "Hi"}
-        doc = {
-            "data": {
-                "_raw": "YXNkZiBibGFoIBIgYmluYXIEAA=="
-            },
-            "type": "payload_telemetry",
-            "receivers": {
-                "TESTCALL": {
-                    "time_created": 1234,
-                    "time_uploaded": 1234
-                }
-            }
-        }
-        doc_id = "cf4511bba32c4273a13d8f2e39501a96" \
-                 "9ec664a4dc5c67bc556b514410087309"
-
         uploader.time.time().AndReturn(1234)
-        self.fake_db.__setitem__(doc_id, doc)
+        self.fake_db.__setitem__(payload_telemetry_doc_id,
+                                 payload_telemetry_doc)
         self.mocker.ReplayAll()
 
-        self.uploader.payload_telemetry(string, metadata)
+        self.uploader.payload_telemetry(payload_telemetry_string,
+                                        payload_telemetry_metadata)
         self.mocker.VerifyAll()
 
-        # TODO: Add last_listener_{info,telem}
+    def ptlm_with_listener_docs(self, doc):
+        doc_metadata = doc["receivers"]["TESTCALL"]
+        listener_telemetry_id = doc_metadata["latest_listener_telemetry"]
+        listener_info_id = doc_metadata["latest_listener_info"]
+
+        # Check that besides the two ids, it equals payload_telemetry_doc
+        del doc_metadata["latest_listener_telemetry"]
+        del doc_metadata["latest_listener_info"]
+
+        try :
+            assert doc == payload_telemetry_doc
+            assert self.docs[listener_telemetry_id]["type"] == \
+                    "listener_telemetry"
+            assert self.docs[listener_info_id]["type"] == "listener_info"
+        except:
+            return False
+        else:
+            return True
+
+    def test_adds_latest_listener_docs(self):
+        self.add_sample_listener_docs()
+
+        uploader.time.time().AndReturn(1234)
+        self.fake_db.__setitem__(payload_telemetry_doc_id, 
+                                 mox.Func(self.ptlm_with_listener_docs))
+        self.mocker.ReplayAll()
+
+        self.uploader.payload_telemetry(payload_telemetry_string,
+                                        payload_telemetry_metadata)
+        self.mocker.VerifyAll()
+
         # TODO: conflict resolution
