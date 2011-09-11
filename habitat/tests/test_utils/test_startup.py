73c1f4ff4e99a68ac503d2d80e13c469ef53b3d9
Daniel Richman <main@danielrichman.co.uk>
2011-09-11 02:12:29 +0100

    Add placeholder test_startup

---
+++ habitat/tests/test_utils/test_startup.py
@@ -0,0 +1,25 @@
+# Copyright 2011 (C) Daniel Richman
+#
+# This file is part of habitat.
+#
+# habitat is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# habitat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with habitat.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Tests for habitat.utils.startup
+
+There are no tests here, since it's probably not useful to have them.
+Tests for the startup functions would largely just be the startup functions
+re-written with Mox, as they just call other modules rather than do
+anything really useful themselves.
+"""
