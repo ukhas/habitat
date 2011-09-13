0b18f1e2d21494fa340bf18c92878ac29e29bb5d
Adam Greig <adam@adamgreig.com>
2011-09-13 22:18:29 +0100

    Update copyright headers on CouchDB functions

--- couchdb/_design/habitat/views/listener_info/map.js
+++ couchdb/_design/habitat/views/listener_info/map.js
@@ -1,24 +1,4 @@
-/*
- * Copyright 2010 (C) Adam Greig
- *
- * This file is part of habitat.
- *
- * habitat is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * habitat is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with habitat.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/* Used by spacenearus_telemetry */
-
+/* Copyright 2010 Adam Greig; Licensed under the GNU GPL v3, see LICENSE */
 function(doc) {
     // Emit a row per current listener_info document containing their callsign
     // and the time this LISTENER_INFO document was created by the listener.
