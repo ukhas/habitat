a21c2914a142b180f5c884e34d169e7297679a54
Adam Greig <adam@adamgreig.com>
2011-12-19 22:08:00 +0000

    flesh out listener_info validation doc

--- couchdb/_design/listener_info/validate_doc_update.js
+++ couchdb/_design/listener_info/validate_doc_update.js
@@ -7,6 +7,37 @@ function(newDoc, oldDoc, userCtx) {
     }
 
     if(oldDoc && !user_id('admin')) {
-        throw({unauthorized: "Only administrators may edit listener_info docs."});
+        throw({unauthorized:
+                "Only administrators may edit listener_info docs."});
+    }
+
+    function required(field, type) {
+        if(!newDoc[field]) {
+            message = "Document must have a `" + field + "` field.";
+            throw({forbidden: message});
+        }
+        if(type && typeof(newDoc[field]) != type) {
+            message = "Wrong type for `" + field + "` ("
+            message += typeof(newDoc[field]) + "), should be " + type + ".";
+            throw({forbidden: message});
+        }
+    }
+
+    required('time_created', 'number');
+    required('time_uploaded', 'number');
+
+    if(newDoc['time_created'] >= newDoc['time_uploaded']) {
+        throw({forbidden:
+                "Document cannot be created after it was uploaded."});
+    }
+
+    required('data', object);
+    
+    if(!newDoc['data']['callsign']) {
+        throw({forbidden: "`data` must contain at least `callsign`."});
+    }
+
+    if(typeof(newDoc['data']['callsign'] != 'string') {
+        throw({forbidden: "`callsign` must be a string."});
     }
 }
