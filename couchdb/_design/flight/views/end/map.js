9513daafb8d034cbf7e341dfbf294c5cf295236e
Daniel Richman <main@danielrichman.co.uk>
2011-12-26 18:28:27 +0000

    Experiments with view names

--- couchdb/_design/flight/views/end/map.js
+++ couchdb/_design/flight/views/end/map.js
@@ -1,5 +1,8 @@
 /* Copyright 2011 (C) Daniel Richman; GNU GPL 3 */
+
 function (doc) {
+    // flights sorted by end time
+
     if (doc.type == "flight")
         emit(doc.end, null);
 }
--- couchdb/_design/listener_info/views/time_created_callsign/map.js
+++ couchdb/_design/listener_info/views/time_created_callsign/map.js
@@ -1,7 +1,6 @@
 /* Copyright 2010 Adam Greig; Licensed under the GNU GPL v3, see LICENSE */
 function(doc) {
-    // Emit a row per current listener_info document containing their callsign
-    // and the time this LISTENER_INFO document was created by the listener.
+    // listener_info sorted by time created, then callsign
     if(doc.type == "listener_info") {
         emit([doc.time_created, doc.data.callsign], null);
     }
