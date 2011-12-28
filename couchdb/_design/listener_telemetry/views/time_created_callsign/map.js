d0e7b51424d0873db012c82f92abd0589594ebcd
Daniel Richman <main@danielrichman.co.uk>
2011-12-28 11:18:54 +0000

    Fix comment in listener_telem view

--- couchdb/_design/listener_telemetry/views/time_created_callsign/map.js
+++ couchdb/_design/listener_telemetry/views/time_created_callsign/map.js
@@ -1,6 +1,6 @@
 /* Copyright 2010 Adam Greig; Licensed under the GNU GPL v3, see LICENSE */
 function(doc) {
-    // listener_info sorted by time created, then callsign
+    // listener_telemetry sorted by time created, then callsign
     if(doc.type == "listener_telemetry") {
         emit([doc.time_created, doc.data.callsign], null);
     }
