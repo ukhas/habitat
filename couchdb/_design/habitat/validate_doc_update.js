d079239b208a92fb35d37997b15b5b590db86eb0
Adam Greig <adam@adamgreig.com>
2012-02-15 23:55:50 +0000

    Move habitat view to python

--- couchdb/_design/habitat/validate_doc_update.js
+++ couchdb/_design/habitat/validate_doc_update.js
@@ -1,16 +1,1 @@
-/* Copyright 2010 Adam Greig; Licensed under the GNU GPL v3, see LICENSE */
-function(newDoc, oldDoc, userCtx) {
-    allowed_types = [
-        "flight", "listener_info", "listener_telemetry", "payload_telemetry"
-    ];
-
-    if(oldDoc && newDoc.type != oldDoc.type)
-        throw({forbidden: "Cannot change document type."});
-
-    for(var i in allowed_types) {
-        if(newDoc.type == allowed_types[i])
-            return;
-    }
-
-    throw({forbidden: "Invalid document type."});
-}
+habitat.views.habitat.validation
---
+++ habitat/views/habitat.py
@@ -0,0 +1,34 @@
+# Copyright 2011 (C) Adam Greig
+#
+# This file is part of habitat.
+#
+# habitat is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# habitat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with habitat.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Provide the core habitat view functions, mostly involving validation.
+"""
+
+from couch_named_python import Forbidden
+
+allowed_types = set(
+    ("flight", "listener_info", "listener_telemetry", "payload_telemetry"))
+
+def validate(new, old, userctx, secobj):
+    if 'type' not in new:
+        raise Forbidden("All documents must have a type.")
+    if new['type'] not in allowed_types:
+        raise Forbidden("Invalid document type.")
+    if old and new['type'] != old['type']:
+        raise Forbidden("Cannot change document type.")
+
