e445abdfb998606360192f7e84e00c6c40054b8b
Daniel Richman <main@danielrichman.co.uk>
2012-05-30 21:49:16 +0100

    UKHASChecksumFixer needs to know about newlines

--- habitat/tests/test_filters.py
+++ habitat/tests/test_filters.py
@@ -24,15 +24,15 @@ from .. import filters as f
 
 class TestFilters:
     def test_semicolons_to_commas(self):
-        data = "$$testpayload,1,2,3;4;5;6*8A24"
+        data = "$$testpayload,1,2,3;4;5;6*8A24\n"
         fixed = f.semicolons_to_commas({}, data)
-        assert fixed == "$$testpayload,1,2,3,4,5,6*888F"
+        assert fixed == "$$testpayload,1,2,3,4,5,6*888F\n"
 
     def test_semicolons_to_commas_with_other_checksum(self):
-        data = "$$testpayload,1,2,3;4;5;6*68"
+        data = "$$testpayload,1,2,3;4;5;6*68\n"
         config = {'checksum': 'xor'}
         fixed = f.semicolons_to_commas(config, data)
-        assert fixed == "$$testpayload,1,2,3,4,5,6*7F"
+        assert fixed == "$$testpayload,1,2,3,4,5,6*7F\n"
 
     def test_numeric_scale(self):
         config = {"source": "key", "factor": (1.0 / 7.0)}
