fbdf871f2365a9fb6e4dd5d80975b1a1fa01c760
Daniel Richman <main@danielrichman.co.uk>
2012-06-28 16:08:05 +0100

    Annotate errors when parsing ukhas fields with field name

--- habitat/parser_modules/ukhas_parser.py
+++ habitat/parser_modules/ukhas_parser.py
@@ -198,7 +198,14 @@ class UKHASParser(ParserModule):
 
         name = config["name"]
         sensor = 'sensors.' + config["sensor"]
-        data = self.loadable_manager.run(sensor, config, field)
+
+        try:
+            data = self.loadable_manager.run(sensor, config, field)
+        except (ValueError, KeyError) as e:
+            # Annotate error with the field name.
+            error_type = e.__class__
+            raise error_type("(field {f}): {e}".format(f=name, e=str(e)))
+
         return [name, data]
 
     def pre_parse(self, string):
